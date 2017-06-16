d10080663881d09e151b62391d2f6d6fcc8a12fc
Lukas A. Mueller <hello@lukasam.com>
2017-06-16 16:24:51 +0200

    Set app up for development

---
+++ Telegraphum/MainController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telegraphum
+//
+//  Created by L on 16/06/2017.
+//  Copyright © 2017 Lukas Mueller. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+}
+
