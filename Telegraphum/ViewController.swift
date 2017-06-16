ea1619678a7de16612a73c028a07243df7eed4db
Lukas A. Mueller <hello@lukasam.com>
2017-06-16 16:18:27 +0200

    Initial Commit

---
+++ Telegraphum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Telegraphum
+//
+//  Created by L on 16/06/2017.
+//  Copyright © 2017 Lukas Mueller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
